0c61c0dc0fc0f4db52cdedafc77fe61b21bf4595
Jose Armando Enriquez Luna <armandoenriquez0531@gmail.com>
2023-02-24 14:27:03 -0600

    Se creo la clase VentanaInicio

--- src/PruebaGUI.java
+++ src/PruebaGUI.java
@@ -1,8 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+
+class VentanaInicio extends JFrame{
+	
+	public VentanaInicio() {
+		
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Programa GUI [Layout Nulo]");
+		setSize(771,580);
+		setLocationRelativeTo(null);
+	}
+}
 
 public class PruebaGUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new VentanaInicio();
+			}
+		});
 
 	}
 
